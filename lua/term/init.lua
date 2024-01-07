a99542247dca08e922d08b9ff49bc966ca1db1bd
Marco Antônio <ma.souza.junior@gmail.com>
2024-01-07 19:11:26 -0300

    fix: move toggle

--- lua/term/init.lua
+++ lua/term/init.lua
@@ -3,20 +3,19 @@ local Term = require("term.term")
 local M = {}
 
 local function set_keymaps(win)
-  local opts = { noremap = true, silent = true }
-
-  vim.keymap.set(
-    { "n", "v", "i", "t" },
-    "C-<leader>",
-    win:toggle,
-    opts
-  )
 end
 
 M.setup = function(opts)
   local term = Term:new()
   term:start(opts.shell or "bash")
-  set_keymaps(term.win)
+
+  local opts = { noremap = true, silent = true }
+  vim.keymap.set(
+    { "n", "v", "i", "t" },
+    "<C-Space>",
+    term:toggle,
+    opts
+  )
 end
 
 return M
