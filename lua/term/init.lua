d0c001429b8269213d0bcd9f9ddc5ca8d5bc9e35
Marco Antônio <ma.souza.junior@gmail.com>
2024-01-07 19:26:05 -0300

    enh: add exit mode command

--- lua/term/init.lua
+++ lua/term/init.lua
@@ -2,14 +2,13 @@ local Term = require("term.term")
 
 local M = {}
 
-local function set_keymaps(win)
-end
-
 M.setup = function(opts)
   local term = Term:new()
   term:start(opts.shell or "bash")
 
   local opts = { noremap = true, silent = true }
+
+  -- open term
   vim.keymap.set(
     { "n", "v", "i", "t" },
     "<C-Space>",
@@ -17,6 +16,11 @@ M.setup = function(opts)
       term:toggle()
     end,
     opts
+  )
+
+  -- exit term mode
+  vim.keymap.set(
+    { "t" }, "<C-x>", "<C-\\><C-n>", opts
   )
 end
 
