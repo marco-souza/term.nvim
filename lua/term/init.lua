ee0fc176900a71269904386bc3283f609e0db127
Marco Antônio <ma.souza.junior@gmail.com>
2024-01-07 19:02:14 -0300

    fix: term keymaps

--- lua/term/init.lua
+++ lua/term/init.lua
@@ -2,21 +2,23 @@ local Term = require("term.term")
 
 local M = {}
 
-local function set_keymaps()
+local function set_keymaps(win)
   local opts = { noremap = true, silent = true }
-  local term_opts = { silent = true }
 
-  vim.api.nvim_set_keymap("n", "C-<leader>", opts)
-  vim.api.nvim_set_keymap("v", "C-<leader>", opts)
-  vim.api.nvim_set_keymap("i", "C-<leader>", opts)
-  vim.api.nvim_set_keymap("t", "C-<leader>", term_opts)
+  vim.keymap.set(
+    { "n", "v", "i", "t" },
+    "C-<leader>",
+    handler,
+    opts
+  )
 end
 
 M.setup = function(opts)
   local term = Term:new()
+
   term:start(opts.shell or "bash")
 
-  set_keymaps()
+  set_keymaps(term.win)
 end
 
 return M
